97a45e5a53949067557a15b3063efba2840bf365
WendolynQuartey <quarteywendolyn@gmail.com>
2026-04-20 11:23:41 -0400

    created an array of quotes

---
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class FamousQuotes {
+    public static void main(String[] args) {
+        String[] quotes = {
+                "The most common way people give up their power is by thinking they don't have any. — Alice Walker",
+                "Define yourself as one who is beautifully made, and divinely placed. — Oprah Winfrey",
+                "I am not free while any woman is unfree, even when her shackles are very different from my own. — Audre Lorde",
+                "You don't make progress by standing on the sidelines, whimpering and complaining. You make progress by implementing ideas. — Shirley Chisholm",
+                "Your silence will not protect you. — Audre Lorde",
+                "I love to see a young girl go out and grab the world by the lapels. Life's a bitch. You've got to go out and kick ass. — Maya Angelou",
+                "Success is liking yourself, liking what you do, and liking how you do it. — Maya Angelou",
+                "If you have no confidence in self, you are twice defeated in the race of life. — Marcus Garvey (often echoed by Black women leaders)",
+                "I have learned over the years that when one's mind is made up, this diminishes fear. — Rosa Parks",
+                "Don't sit down and wait for the opportunities to come. Get up and make them. — Madam C.J. Walker"
+        };
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number between 1 and 10 to display a quote: ");
+        int userChoice = scanner.nextInt();
+
+    }
+}
